1daaeb6fabdc92b1a20f9bc29896d7a39f229c72
Alex Zhao <zshalex@gmail.com>
2012-03-29 17:18:15 +0800

    添加文档
    Signed-off-by: Alex Zhao <zshalex@gmail.com>

--- include/testcase.h
+++ include/testcase.h
@@ -1,8 +1,13 @@
+/** @file testcase.h
+*
+*/
+
 /**
-  @defgroup QTEST-PLUS
-  @author Alex Zhao
-  @version 1.0
-  @date 2012-03-29
+*  @defgroup QTEST-PLUS
+*  @author Alex Zhao
+*  @version 1.0
+*  @date 2012-03-29
+*  @{
 */
 
 #ifndef TESTCASE_H
@@ -16,33 +21,44 @@
 namespace ZSHALEX_QTEST_PLUS {
 
 /**
-  声明一个单元测试类的静态变量,并添加一个构造函数.
+*  @name 宏定义
+*  @{
+*/
+
+/** @def DECLARE_TEST_CASE(type)
+*  声明一个单元测试类的静态变量,并添加一个构造函数.
 */
 #define DECLARE_TEST_CASE(type) \
     type(const QString &name):TestCase(name) {} \
     static type tc
 
-/** @brief 初始化单元测试的静态变量.*/
+/** @def INIT_TEST_CASE(type, name)
+*  初始化单元测试的静态变量.
+*/
 #define INIT_TEST_CASE(type, name) \
     type type::tc(name)
 
 /**
-  单元测试的基类.在创建单元测试的实例时,TestCase会自动的将自己注册到
-  TestSuite中去.
+  @}
+*/
+
+/** @class TestCase
+*  单元测试的基类.在创建单元测试的实例时,TestCase会自动的将自己注册到
+*  TestSuite中去.
 */
 class QTESTPLUSSHARED_EXPORT TestCase : public QObject
 {
     Q_OBJECT
 public:
     /**
-      构造函数,向TestSuite中注册自己.
-      @param name 单元测试的名称.
+    *  构造函数,向TestSuite中注册自己.
+    *  @param name 单元测试的名称.
     */
     explicit TestCase(const QString &name);
 
     /**
-      获取单元测试的名称.
-      @return 单元测试的名称.
+    *  获取单元测试的名称.
+    *  @return 单元测试的名称.
     */
     inline QString name() {return m_name;}
 protected:
@@ -52,3 +68,6 @@ protected:
 }
 
 #endif // TESTCASE_H
+/**
+*  @}
+*/
--- include/testsuite.h
+++ include/testsuite.h
@@ -1,8 +1,9 @@
-/**
-  @defgroup QTEST-PLUS
-  @author Alex Zhao
-  @version 1.0
-  @date 2012-03-29
+/** @file testsuite.h
+*  @defgroup QTEST-PLUS
+*  @author Alex Zhao
+*  @version 1.0
+*  @date 2012-03-29
+*  @{
 */
 
 #ifndef TESTSUITE_H
@@ -17,16 +18,16 @@
 
 namespace ZSHALEX_QTEST_PLUS {
 
-/**
-  单元测试套件,它用于保存所有注册的单元测试,并控制单元测试的执行.
+/** @class TestSuite
+*  单元测试套件.用于保存所有注册的单元测试,并控制单元测试的执行.
 */
 class QTESTPLUSSHARED_EXPORT TestSuite : public QObject
 {
     Q_OBJECT
 public:
     /**
-      单元测试套件的单体.
-      @return 返回单元测试套件的单体.
+    *  单元测试套件的单体.
+    *  @return 返回单元测试套件的单体.
     */
     static TestSuite * instance();
 
@@ -34,35 +35,35 @@ public:
     ~TestSuite();
 
     /**
-      将单元测试注册到单元测试套件中
-      @param testCase 需要注册的单元测试.
+    *  将单元测试注册到单元测试套件中
+    *  @param testCase 需要注册的单元测试.
     */
     void addTestCase(TestCase *testCase);
     /**
-      根据命令行参数执行套件中的单元测试.
-      @param argc 参数个数
-      @param argv 参数数组
+    *  根据命令行参数执行套件中的单元测试.
+    *  @param argc 参数个数
+    *  @param argv 参数数组
     */
     int execute(int argc, char** argv);
 
     /**
-      获取单元测试的总数.
-      @return 单元测试的总数.
+    *  获取单元测试的总数.
+    *  @return 单元测试的总数.
     */
     inline int totalCount(){return m_totleCount;}
     /**
-      获取错误单元测试的个数.
-      @return 错误单元测试的个数.
+    *  获取错误单元测试的个数.
+    *  @return 错误单元测试的个数.
     */
     inline int errorCount(){return m_errorCount;}
     /**
-      获取成功单元测试的个数.
-      @return 成功单元测试的个数.
+    *  获取成功单元测试的个数.
+    *  @return 成功单元测试的个数.
     */
     inline int successCount(){return m_successCount;}
     /**
-      获取已经执行的单元测试的个数.
-      @return 已经执行的单元测试的个数.
+    *  获取已经执行的单元测试的个数.
+    *  @return 已经执行的单元测试的个数.
     */
     inline int runCount(){return m_runCount;}
 private:
@@ -84,5 +85,9 @@ private:
 };
 
 }
+
+/**
+  @}
+*/
 
 #endif // TESTSUITE_H
